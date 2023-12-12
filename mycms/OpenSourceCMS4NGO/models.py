849553246ae758e5ba1ccd82371c1ed5c89a1f97
Daniel Malinowski <151693844+Daniel-Malinowski@users.noreply.github.com>
2023-12-12 13:49:27 +0000

    modified:   mycms/OpenSourceCMS4NGO/models.py

---
+++ mycms/OpenSourceCMS4NGO/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Admin(models.Model):
+    name = models.CharField(
+        max_length=100, blank=False, verbose_name='Nazwa Użytkownika')
+    email = models.EmailField(
+        max_length=100, blank=False, verbose_name='Adres email')
+    password = models.CharField(
+        min_length=8, max_length=16, blank=False, verbose_name='Hasło')
+
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Użytkownik'
+        verbose_name_plural = 'Użytkownicy'
+        ordering = ['name']
+        
